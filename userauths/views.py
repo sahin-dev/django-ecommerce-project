04f56ad8375504409cf8f28f140343a0ecabb653
NafisHere <abujorshabbib@gmail.com>
2024-07-17 14:26:19 +0600

    template changed

--- userauths/views.py
+++ userauths/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.conf import settings
 from django.contrib.auth import get_user_model
+
 
 User = get_user_model()
 
@@ -15,7 +16,7 @@ def register_view(request):
             new_user = form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Hey {username}, your account was created successfully.")
-            new_user = authenticate(username=username, password=form.cleaned_data['password1'])
+            new_user = authenticate(username=form.cleaned_data['email'], password=form.cleaned_data['password1'])
             if new_user is not None:
                 login(request, new_user)
                 return redirect("core:index")
@@ -50,7 +51,16 @@ def login_view(request):
             messages.success(request, "You are logged in.")
             return redirect("core:index")
         else:
-            messages.warning(request, "Invalid credentials, please try again.")
+            messages.warning(request, "User Doestn't exist, creat an account. ")
     
-    context = {}
+    context = {
+
+    }
     return render(request, "userauths/sign-in.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You Logged Outtt!. ")
+    return redirect("userauths:sign-in")
+  
